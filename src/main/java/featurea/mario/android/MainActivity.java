ba4a328c1da85f790003ca47cf3bb33c60d3a111
Dmitry Kolesnikovich <kolesnikovichdn@gmail.com>
2017-07-10 02:55:35 +0300

    initial commit

---
+++ src/main/java/featurea/mario/android/MainActivity.java
@@ -0,0 +1,15 @@
+package featurea.mario.android;
+
+import featurea.android.FeatureaActivity;
+import featurea.app.Application;
+import featurea.util.FileUtil;
+
+public class MainActivity extends FeatureaActivity {
+
+    @Override
+    public void onCreateApplication(Application app) {
+        super.onCreateApplication(app);
+        FileUtil
+    }
+
+}
