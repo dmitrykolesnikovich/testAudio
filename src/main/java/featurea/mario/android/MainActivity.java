bc9f7a5c7f1b2cdd9a32f648ac9f2c71c4b77fd1
Dmitry Kolesnikovich <kolesnikovichdn@gmail.com>
2017-07-10 03:00:41 +0300

    initial commit

--- src/main/java/featurea/mario/android/MainActivity.java
+++ src/main/java/featurea/mario/android/MainActivity.java
@@ -1,15 +1,27 @@
 package featurea.mario.android;
 
-import featurea.android.FeatureaActivity;
-import featurea.app.Application;
+import android.app.Activity;
+import android.os.Bundle;
+import featurea.android.util.ApkFileUtil;
 import featurea.util.FileUtil;
 
-public class MainActivity extends FeatureaActivity {
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.JarFile;
+
+public class MainActivity extends Activity {
 
     @Override
-    public void onCreateApplication(Application app) {
-        super.onCreateApplication(app);
-        FileUtil
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            JarFile jarFile = ApkFileUtil.getInstance().getFile(getApplication());
+            InputStream inputStream = jarFile.getInputStream(jarFile.getEntry("Castle.mp3"));
+            FileUtil.copyPaste(inputStream, new File("temp/Castle.mp3"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
